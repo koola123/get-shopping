a61b5f0bb9919f313e5a4d843f389139511eed54
Matthias Müringer <mmueringer@web.de>
2023-04-14 01:22:46 +0200

    update: cartItem component

--- src/components/cart-item.jsx
+++ src/components/cart-item.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 // import link
 import { Link } from 'react-router-dom';
 // import icons
-import { IoMdClose } from 'react-icons/io';
+import { IoMdClose, IoMdRemove } from 'react-icons/io';
 
 export const CartItem = ({ item }) => {
   // destructure the item
@@ -14,21 +14,40 @@ export const CartItem = ({ item }) => {
         <Link to={`/product/${id}`}>
       <img className="max-w-[80px]" src={image} alt="" />
         </Link>
+        <div className="w-full flex flex-col">
         <div>
         {/*title & remove icon*/}
-        <div>
+        <div className="flex justify-between mb-2">
+          {/* title */}
+          <Link className="text-sm uppercase font-medium max-w-[240px]
+          text-primary hover:underline" to={`/product/${id}`}>
+          </Link>
         {/*title*/}
           <Link to={`/product/${id}`}
-          className="text-sm uppercase font-medium max-w-[240px] text-primary hover:underline">
+          className="text-sm uppercase font-medium max-w-[240px]
+           text-primary hover:underline"
+           >
           {title}
           </Link>
           {/*remove icon*/}
+          <div className="text-xl cursor-pointer">
+            <IoMdClose className="text-gray-500 hover:text-red-500 transition"/>
+            </div>
+        </div>
+        <div className="bg-orange-200 flex gap-x-2 h-[36px] text-sm ">
+          {/* qty */}
           <div>
-          <IoMdClose />
+            {/* minus icon */}
+            <IoMdRemove/>
           </div>
+           {/* item price */}
+           <div>item price</div>
+             {/* item price */}
+           <div>final price</div>
         </div>
         </div>
+      </div>
     </div>
-    </div>
+  </div>
   );
 };
